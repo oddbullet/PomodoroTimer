52ace4c428ce919be3f8d4df0043abbb6e6496e1
Bill Yang <joyang12345@gmail.com>
2024-10-27 16:14:03 -0400

    inital

---
+++ pomodoro-timer/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Timer from './components/Timer.vue'
+
+</script>
+
+<template>
+  <Timer></Timer>
+</template>
+
+<style scoped>
+</style>
---
+++ pomodoro-timer/src/components/Timer.vue
@@ -0,0 +1,5 @@
+<script></script>
+<template>
+    <h1>00:00</h1>
+</template>
+<style scoped></style>
